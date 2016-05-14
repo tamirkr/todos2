327fb5a29152ef5f7d702425d76bef41668296cf
Tamir Kratz <tamirkratz@gmail.com>
2016-05-14 23:03:45 +0300

    Initial commit

---
+++ app.js
@@ -0,0 +1,111 @@
+/**
+ * Created by Tamir on 14/05/2016.
+ */
+"use strict";
+
+var app = angular.module("app",['ngStorage']);
+
+app.component('myTodos',{
+    templateUrl:'myTodos.html',
+    controller:function (storageService) {
+        var ctrl = this;
+        ctrl.title = 'My Todos';
+        ctrl.task = '';
+
+        ctrl.$onInit = function () {
+            ctrl.todos = storageService.get();
+        }
+        
+        ctrl.complete = storageService.countComplete();
+
+
+        ctrl.add = function () {
+
+            if(ctrl.task == undefined || ctrl.task.trim().length == 0){
+                return;
+            }
+
+            storageService.add({id:ctrl.todos.length,name:ctrl.task});
+            ctrl.task = undefined;
+        }
+
+        ctrl.remove = function (index) {
+            storageService.remove(index);
+            ctrl.complete = storageService.countComplete();
+        }
+
+
+        
+        ctrl.markAsDone = function (todo) {
+            todo.done = todo.done ? false : true;
+            ctrl.complete = storageService.countComplete();
+
+        }
+
+        ctrl.total = function () {
+            return ctrl.todos.length + ' - total';
+        }
+
+        ctrl.markAll = function () {
+            storageService.markAll();
+            ctrl.complete = storageService.countComplete();
+        }
+
+
+
+    }
+});
+
+
+
+app.factory('storageService',storageService);
+storageService.$inject = ['$localStorage'];
+function storageService($localStorage) {
+    var markall = false;
+    $localStorage = $localStorage.$default({
+        todos: {}
+    });
+
+    var _get = function () {
+        return $localStorage.todos;
+    }
+
+    var _remove = function (todo) {
+        $localStorage.todos.splice($localStorage.todos.indexOf(todo),1);
+    }
+
+    var _add = function (todo) {
+        $localStorage.todos.unshift(todo);
+    }
+
+    var _markAll = function () {
+        for(var i=0;i<$localStorage.todos.length;i++){
+            if(!markall){
+                $localStorage.todos[i].done = true;
+            }
+            else{
+                $localStorage.todos[i].done = false;
+            }
+        }
+        markall = !markall;
+    }
+
+    var _countComplete = function() {
+        var num = 0;
+        for(var i = 0; i < $localStorage.todos.length; i++){
+            if($localStorage.todos[i].done) {
+                num++;
+            }
+        }
+        return num;
+    }
+
+    return{
+        get:_get,
+        add:_add,
+        remove:_remove,
+        countComplete : _countComplete,
+        markAll:_markAll
+    };
+}
+
